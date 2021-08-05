d04b224463743953b894299da2e4390c1b4fed99
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-08-05 14:37:37 +0900

    Initial Commit

---
+++ Firebase-Authentication/Firebase_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_AuthenticationApp.swift
+//  Firebase-Authentication
+//
+//  Created by administrator on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
